cf3f6617978d4e0de06f3c9a91f747aa34b73d1c
Nam Vinh <vinhdn@biso24.com>
2024-09-08 01:03:21 +0700

    feat: complete conding test

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import React, { useEffect, useState } from "react";
+import { Button, Card, Carousel, Col, Image, Input, Row, Typography } from "antd";
+import { useCallback, useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import Slider from "react-slick";
-import { Response } from "./service/API_GET_Employees.json";
+import "./App.css"; // Import your CSS for responsiveness
+import { fetchEmployees } from "./service";
+import { useDebounceCallback } from "usehooks-ts";
+import { Employee, Id, Position } from "./types/models/Employee";
 
-function App() {
-  const [employees, setEmployees] = useState([]);
+export const PAGE_SIZE = 8;
+
+const App = () => {
+  const [employees, setEmployees] = useState<Employee[]>([]);
   const [page, setPage] = useState(1);
 
+  const loadEmployees = useCallback(
+    async (search?: string) => {
+      const response = await fetchEmployees(page, PAGE_SIZE, search);
+
+      setEmployees((prev) => (search ? response.data.pageItems : [...new Set([...prev, ...response.data.pageItems])]));
+    },
+    [page]
+  );
+
   useEffect(() => {
-    // Simulate fetching data
-    fetchEmployees(page);
-  }, [page]);
+    loadEmployees();
+  }, [loadEmployees, page]);
 
-  const fetchEmployees = async (pageNumber) => {
-    // Mock data or fake API call here
-    const response = Response;
-    const sortedData = response.data.pageItems.sort((a, b) => {
-      const yearsA = calculateYearsOfExperience(a.positions);
-      const yearsB = calculateYearsOfExperience(b.positions);
-      return yearsB - yearsA;
-    });
-    setEmployees((prev) => [...prev, ...sortedData]);
-  };
-
-  const calculateYearsOfExperience = (positions) => {
+  const calculateYearsOfExperience = (positions: Position[]) => {
     return positions.reduce((total, position) => {
       return (
         total +
@@ -34,25 +36,68 @@ function App() {
     }, 0);
   };
 
-  const deleteEmployee = (id) => {
+  const deleteEmployee = (id: Id) => {
     setEmployees(employees.filter((emp) => emp.id !== id));
   };
 
+  const onSearch = (value: string) => loadEmployees(value);
+
   return (
-    <InfiniteScroll dataLength={employees.length} next={() => setPage(page + 1)} hasMore={true}>
-      {employees.map((employee) => (
-        <div key={employee.id} className='employee-card'>
-          <h3>{employee.name}</h3>
-          <button onClick={() => deleteEmployee(employee.id)}>Delete</button>
-          <Slider>
-            {employee.positions[0].toolLanguages[0].images.map((image) => (
-              <img key={image.id} src={image.cdnUrl} alt='Portfolio' />
-            ))}
-          </Slider>
-        </div>
-      ))}
-    </InfiniteScroll>
+    <div className='employee-list'>
+      <Typography.Text ellipsis={{ tooltip: true }} className='label m-2'>
+        List employess
+      </Typography.Text>
+      <div className='flex items-center gap-2'>
+        <Typography.Text ellipsis={{ tooltip: true }}>Search name:</Typography.Text>
+        <Input.Search
+          className='input-search'
+          placeholder='input search text'
+          onSearch={useDebounceCallback(onSearch, 500)}
+          enterButton
+        />
+      </div>
+
+      <InfiniteScroll
+        dataLength={employees?.length}
+        next={() => setPage(page + 1)}
+        hasMore={true}
+        loader={!employees?.length && <h4>Loading...</h4>}
+      >
+        <Row>
+          {employees?.map((employee) => (
+            <Col span={8} xs={24} sm={12} md={8} key={employee?.id}>
+              <Card size='small' className='employee-card'>
+                <Carousel>
+                  {employee?.positions?.[0]?.toolLanguages?.[0]?.images?.map((image) => (
+                    <Image.PreviewGroup key={image?.id} items={image?.cdnUrls}>
+                      <Image width={"100%"} height={"100%"} src={image?.cdnUrl} className='image' />
+                    </Image.PreviewGroup>
+                  ))}
+                </Carousel>
+                <div className='flex justify-between m-2'>
+                  <Typography.Text ellipsis={{ tooltip: true }} className='title'>
+                    {employee?.name}
+                  </Typography.Text>
+                  <Typography.Text ellipsis={{ tooltip: true }} className='text'>
+                    {calculateYearsOfExperience(employee?.positions)} years
+                  </Typography.Text>
+                </div>
+                <Typography.Text ellipsis={{ tooltip: true }} className='text flex flex-column text-start m-2'>
+                  {employee?.positions?.[0]?.name}
+                </Typography.Text>
+                <Typography.Text ellipsis={{ tooltip: true }} className='text flex flex-column text-start m-2'>
+                  {employee?.positions?.[0]?.toolLanguages?.[0]?.description}
+                </Typography.Text>
+                <Button className='delete-btn' onClick={() => deleteEmployee(employee?.id)}>
+                  Delete
+                </Button>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </InfiniteScroll>
+    </div>
   );
-}
+};
 
 export default App;
